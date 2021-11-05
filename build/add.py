73fee4b99cedc8add38e7ecf605e0c11123801fd
karthikeyanrathore <karthikerathore@gmail.com>
2021-11-05 23:13:16 +0530

    multidigit addition added

--- build/add.py
+++ build/add.py
@@ -2,6 +2,7 @@
 import math
 INTEGER="INTEGER"
 PLUS="PLUS"
+MINUS="MINUS"
 EOF= "EOF"
 
 class Token(object):
@@ -17,18 +18,41 @@ class Interpreter(object):
     self.current_token = None
     self.pos = 0
   
+  def forward(self):
+    if self.pos >= len(self.str) - 1 :
+      return 0
+    else:
+      self.pos += 1
+      return 1
+
+  def multidigit(self):
+    sum = ""
+    current_char = self.str[self.pos]
+    while current_char.isdigit():
+      sum+= str(current_char)
+      if self.forward():
+        current_char = self.str[self.pos]
+      else:
+        self.pos += 1
+        break
+    return sum
+    
   def get_token(self):
     if self.pos > len(self.str) - 1:
       return Token(None, EOF)
 
     current_char = self.str[self.pos]
     if current_char.isdigit():
-      token = Token(INTEGER, int(current_char))
-      self.pos += 1
+      token = Token(INTEGER,self.multidigit())
       return token
 
     if current_char == "+":
       token = Token(PLUS, (current_char))
+      self.pos += 1
+      return token
+    
+    if current_char == "-":
+      token = Token(MINUS, (current_char))
       self.pos += 1
       return token
   
@@ -51,14 +75,19 @@ class Interpreter(object):
     
     mid = self.current_token
     print("mid", self.current_token)
-    self.push_check(PLUS)
+    if self.current_token.type == PLUS:
+      self.push_check(PLUS)
+    if self.current_token.type == MINUS:
+      self.push_check(MINUS)  
 
     right = self.current_token
     print("right", self.current_token)
     self.push_check(INTEGER)
     
-    return left.value  + right.value
-    
+    if mid.type == PLUS:
+      return int(left.value)  + int(right.value)
+    if mid.type == MINUS:
+      return int(left.value)  -  int(right.value)
 
 
 
